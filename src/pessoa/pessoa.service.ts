fb412514c1467a692f927610c381b9456ecc6aff
Matheus <33672883+gsmatheus@users.noreply.github.com>
2022-04-28 00:16:33 -0300

    feat: Nova função que busca todos os exames de uma pessoa

--- src/pessoa/pessoa.service.ts
+++ src/pessoa/pessoa.service.ts
@@ -4,6 +4,7 @@ import { PessoaRepository } from './pessoa.repository';
 import { CreatePessoaDto } from './dtos/create-pessoa.dto';
 import { Pessoa } from './pessoa.entity';
 import { from, Observable } from 'rxjs';
+import { Exame } from '../exame/exame.entity';
 
 /**
  * @Injectable que é o responsável por fazer com que nossa classe faça parte
@@ -26,5 +27,32 @@ export class PessoaService {
     // if (!pessoa) throw new NotFoundException('Pessoa não encontrada.');
 
     return pessoa;
+  }
+
+  async findAllExames(pessoaId: string): Promise<Exame[]> {
+    // const pessoa = await this.pessoaRepository.find({
+    //   relations: ['exames'],
+    //   where: { id: pessoaId },
+    // });
+
+    // const pessoa = await this.pessoaRepository
+    //   .createQueryBuilder('pessoa')
+    //   .leftJoinAndSelect('pessoa.exames', 'exame')
+    //   .where('id', {})
+    //   .getMany();
+
+    const pessoa = await this.pessoaRepository.find({
+      join: {
+        alias: 'pessoa',
+        leftJoinAndSelect: {
+          exame: 'pessoa.exames',
+        },
+      },
+      where: {
+        id: '9df24822-3cdb-4b3c-b35c-03465dd443db',
+      },
+    });
+
+    return pessoa[0].exames;
   }
 }
